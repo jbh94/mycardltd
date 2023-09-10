90cae8c59e23024ecf2b993525fe6e7792461ad8
joe <joey2246@hotmail.com>
2023-09-10 22:04:51 +0100

    burger menu added

--- ui/src/app/components/navigation/navigation.component.ts
+++ ui/src/app/components/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { AuthService } from 'src/app/services/auth.service';
@@ -11,6 +11,8 @@ import { AuthService } from 'src/app/services/auth.service';
 export class NavigationComponent implements OnInit {
   isAuthenticated: Boolean = false;
 
+  isMenuVisible: boolean = true;
+
   ngOnInit(): void {
     this.authService.isUserLoggedIn$.subscribe((isLoggedIn: Boolean) => {
       this.isAuthenticated = isLoggedIn
@@ -18,6 +20,28 @@ export class NavigationComponent implements OnInit {
   }
 
   constructor(private authService: AuthService, private router: Router) { }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.updateMenuVisibility();
+  }
+
+  toggleMenu() {
+    const menu = document.querySelector('#menu') as HTMLElement;
+    menu.classList.toggle('hidden');
+  }
+
+  updateMenuVisibility() {
+    const screenWidth = window.innerWidth;
+    if (screenWidth >= 768) {
+      this.isMenuVisible = true;
+    } else {
+      const menu = document.querySelector('#menu') as HTMLElement;
+      if (!menu.classList.contains('hidden')) {
+        this.isMenuVisible = true;
+      }
+    }
+  }
 
   logout() {
     localStorage.removeItem('token');
